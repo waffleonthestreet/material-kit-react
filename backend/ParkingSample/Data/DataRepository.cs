91e362dceca02c3e3d0f7191c1e83434a01f1d36
waffleonthestreet <woojeongkim610@gmail.com>
2021-09-10 17:05:04 +0900

    working on DataRepository

---
+++ backend/ParkingSample/Data/DataRepository.cs
@@ -0,0 +1,58 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using ParkingSample.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkingSample.Data
+{
+    public class DataRepository : IDataRepository
+    {
+        private readonly string _connectionString;
+
+        public DataRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration["ConnectionsStrings:DefaultConnection"];
+        }
+
+        public Question GetQuestion(int questionId)
+        {
+            using (var connection = new SqlConnection(_connectionString) )
+            {
+                connection.Open();
+                return connection.QueryFirstOrDefault<Question>("SELECT * FROM Question WHERE QuestionId = @QuestionId", new { QuestionId = questionId});
+            }
+        }
+
+        public IEnumerable<Question> GetQuestions()
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                return connection.Query<Question>(@"SELECT * FROM Question");
+            }
+        }
+
+        public IEnumerable<Question> GetQuestionsBySearch(string search)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                return connection.Query<Question>(@"SELECT * FROM Question WHERE TITLE LIKE '%' + @Search + '%' OR CONTENT LIKE '%' + @Search + '%'", new { Search = search });
+            }
+        }
+
+        public bool QuestionExists(int questionId)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                return connection.QueryFirst<bool>(@"SELECT CASE WHEN EXISTS (SELECT QuestionId FROM dbo.Question WHERE QuestionId = @QuestionId) THEN CAST (1 AS BIT) ELSE CAST (0 AS BIT) END AS Result", 
+                    new { QuestionId = questionId });
+            }
+        }
+    }
+}
---
+++ backend/ParkingSample/Data/IDataRepository.cs
@@ -0,0 +1,19 @@
+﻿using ParkingSample.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkingSample.Data
+{
+    public interface IDataRepository
+    {
+        IEnumerable<Question> GetQuestions();
+
+        IEnumerable<Question> GetQuestionsBySearch(string search);
+
+        Question GetQuestion(int questionId);
+
+        bool QuestionExists(int questionId);
+    }
+}
